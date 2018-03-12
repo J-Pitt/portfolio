7c5c739d0c2d90090188004a7b3ce39697f783a9
J-pitt <jason.pittman7@gmail.com>
2018-03-11 22:18:14 -0700

    fixing configs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to my Portfolio Site!!</h1>
         </header>
         <p className="App-intro">
-          AdTech professional with over 3 years industry experience. Helping publishers and content creators optimize their advertising stack, creating efficient hybrid (client-side/server-side) header bidding solutions 
+          AdTech professional with over 3 years industry experience. Helping publishers and content creators optimize their digital advertising by helping them leverage demand from Amazon's Transparent Ad Marketplace. 
         </p>
       </div>
     );
--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,10 @@ class Home extends Component {
         <h2>Work History
         </h2>
         <p>My name is Jason and I have been working in the adops and adtech engineering industry for over three years. I got my 
-        start at sovrn holdings llc in their adops department, working with our account management and sales team to help publishers
+        start at sovrn holdings llc in their adops department, working with the account management and sales teams to help publishers
         create ad tags, align demand, and identify and remedy any ad serving issues. I then helped build out their support engineering 
         team to provide support for prebid.js, along with other engineering related support. In addition to provide header bidding support, 
-        I also assited with DSP integrations, pixel tracking configuration, and container tag management.</p>
+        I also assited our ad trafficking team with DSP integrations, pixel tracking and cookiesync integrations, and container tag management.</p>
  
         <p>After that, I transitioned to a sales engineering role with Amazon Publisher Servies, within the A9 unit. Here, I have been 
         primarily supporting both their Transparent Ad Marketplace and Unified Ad Marketplace products, helping publishers integrate successfully, 
